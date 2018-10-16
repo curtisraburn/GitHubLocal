8cc5d7d148f0c656ab27a174ec3a9d26d1c849cf
curtisraburn <44200519+curtisraburn@users.noreply.github.com>
2018-10-16 09:36:41 -0400

    Update ADJoin.ps1

--- ADJoin.ps1
+++ ADJoin.ps1
@@ -1,6 +1,1 @@
-$domain = "autobuild.com"
-$password = "C0ncerto!!!!" | ConvertTo-SecureString -asPlainText -Force
-$username = "$autobuild\concerto" 
-$credential = New-Object System.Management.Automation.PSCredential($username,$password)
-Add-Computer -DomainName $domain -Credential $credential 
 restart-computer -computername "custscrptextn" -Force
